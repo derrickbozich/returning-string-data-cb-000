f1e4cb0dec83352cec1c073be7f56fdac55d7174
derrick b <dbozich1@gmail.com>
2018-10-16 18:18:36 +0000

    automatically backed up by learn

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -6,12 +6,12 @@
 //   })
 // })
 
-$(function () {
-  $(".js-more").on('click', function() {
-    // get the id from the data attribute
-    var id = $(this).data("id");
-    $.get("/posts/" + id + "/body", function(data) {
-      alert(data);
-    });
-  });
-});
+// $(function () {
+//   $(".js-more").on('click', function() {
+//     // get the id from the data attribute
+//     var id = $(this).data("id");
+//     $.get("/posts/" + id + "/body", function(data) {
+//       alert(data);
+//     });
+//   });
+// });
