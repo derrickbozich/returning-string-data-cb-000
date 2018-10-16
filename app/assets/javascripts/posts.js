e848daaf24c245a3ba171a4cf013e78d4fba2f93
derrick b <dbozich1@gmail.com>
2018-10-16 18:16:17 +0000

    automatically backed up by learn

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,7 +1,17 @@
-$(function(){
-  $("[data-id='8']").on('click', function(e){
-    e.preventDefault
-    console.log('stop')
-    debugger
-  })
-})
+// $(function(){
+//   $("[data-id='8']").on('click', function(e){
+//     e.preventDefault
+//     console.log('stop')
+//     debugger
+//   })
+// })
+
+$(function () {
+  $(".js-more").on('click', function() {
+    // get the id from the data attribute
+    var id = $(this).data("id");
+    $.get("/posts/" + id + "/body", function(data) {
+      alert(data);
+    });
+  });
+});
