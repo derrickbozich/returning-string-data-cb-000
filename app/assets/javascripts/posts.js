9d586c1d3eabb13e5d1a0bcb252d7eec807d38e1
derrick b <dbozich1@gmail.com>
2018-10-16 18:31:36 +0000

    automatically backed up by learn

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,6 +2,7 @@ $(function(){
   $(".js-more").on('click', function(e){
     e.preventDefault
     let url = this.attributes.action.value
+    debugger
     $.get(url, function(data){
       debugger
       // $(`#body-${}`).html(data)
